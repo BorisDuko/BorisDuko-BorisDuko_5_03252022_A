830abab7c9fcb8498cc2c329367ef60b247850da
BorisDuko <boris.duko@greta-cfa-aquitaine.academy>
2022-09-27 19:33:30 +0200

    test loop for colors array

--- front/js/codeTemplate.js
+++ front/js/codeTemplate.js
@@ -30,3 +30,30 @@ fetch(apiURL)
       itemContainer.insertAdjacentHTML("beforeend", html);
     });
   });
+
+// URL Search Params example
+console.log("Window Location:", window.location);
+
+const myKeyValue = window.location.search;
+console.log("Keys & Values:", myKeyValue);
+
+const urlParams = new URLSearchParams(myKeyValue);
+
+const paramId = urlParams.get("id");
+
+console.log("Id:", paramId);
+
+// forEach method order example
+const movements = [200, 450, -400, 300, -650, -130, 70, 1300];
+movements.forEach((element, index, arr) => {
+  if (element > 0) {
+    console.log(`Movement #${index + 1}: ${element}`);
+  } else {
+    console.log(`#${index + 1} Withdraw: ${Math.abs(element)}`);
+  }
+});
+
+
+// dropdown items
+<option value="vert">${product.colors[0]}</option>
+<option value="blanc">${product.colors[1]}</option>
--- front/js/product.js
+++ front/js/product.js
@@ -1,28 +1,53 @@
 // Get access to the DOM
 const itemContainer = document.querySelector(".item");
 
-const productId = "415b7cacb65d43b2b5c1ff70f3393ad1";
+const colorDropdown = document.querySelector("#colors");
 
-// API URL
-const apiURL = "http://localhost:3000/api/products/" + productId;
-console.log(apiURL);
+// URL Search Params
+const productKeyValue = window.location.search;
+const productUrlParams = new URLSearchParams(productKeyValue);
+const productParamId = productUrlParams.get("id");
+
+// API URL: / + product Id
+const apiUrlId = "http://localhost:3000/api/products/" + productParamId;
+
+// ----- to see product info -----
+fetch(apiUrlId)
+  .then((res) => {
+    return res.json();
+  })
+  .then((productInfo) => {
+    console.log("Array of product info: ⤵", productInfo);
+  });
+// ----- to see product info -----
 
 // Render product page
-fetch(apiURL)
+fetch(apiUrlId)
   .then((response) => {
     return response.json();
   })
   .then((product) => {
+    // try for loop for colors
+    // console.log(product);
+    // product.colors.forEach((element, index) => {
+    //   const markup = `
+    //   <option value="${element.colors}">${element.colors}</option>
+    //   `;
+    //     document
+    //       .querySelector("select")
+    //       .insertAdjacentHTML("beforeend", markup);
+    // });
+    // //////////////////
     const html = `
       <article>
             <div class="item__img">
-              <!-- <img src="../images/logo.png" alt="Photo of a sofa"> -->
+            <img src=${product.imageUrl} alt=${product.altTxt}>
             </div>
             <div class="item__content">
               <div class="item__content__titlePrice">
                 <h1 id="title">${product.name}</h1>
                 <p>
-                  Prix : <span id="price">${product.price}</span>€
+                  Price : <span id="price">${product.price}</span>€
                 </p>
               </div>
 
@@ -31,7 +56,7 @@ fetch(apiURL)
                   Description:
                 </p>
                 <p id="description">
-                  <!-- Dis enim malesuada risus sapien gravida nulla nisl arcu. -->
+                  ${product.description}
                 </p>
               </div>
 
@@ -40,8 +65,12 @@ fetch(apiURL)
                   <label for="color-select">Chose your color:</label>
                   <select name="color-select" id="colors">
                     <option value="">--Please, select a color --</option>
-                    <!--                       <option value="vert">green</option>
-                      <option value="blanc">white</option> -->
+                    //////////////////////////////
+
+                    <option value="vert">green</option>
+                    <option value="blanc">white</option>
+                      
+                    /////////////////////////////////
                   </select>
                 </div>
 
@@ -65,5 +94,37 @@ fetch(apiURL)
             </div>
           </article>
       `;
-    itemContainer.insertAdjacentHTML("beforeend", html);
+    // itemContainer.insertAdjacentHTML("beforeend", html);
+    document
+      .querySelector("section")
+      .insertAdjacentHTML("beforeend", html);
   });
+
+// forEach method order example
+// const movements = [200, 450, -400, 300, -650, -130, 70, 1300];
+// movements.forEach((element, index, arr) => {
+//   if (element > 0) {
+//     console.log(`Movement #${index + 1}: ${element}`);
+//   } else {
+//     console.log(`#${index + 1} Withdraw: ${Math.abs(element)}`);
+//   }
+// });
+
+const addColorToDropdown = () => {
+  fetch(apiUrlId)
+    .then((res) => {
+      return res.json();
+    })
+    .then((productInfo) => {
+      productInfo.forEach((element) => {
+        const markup = `
+        <option value="${element.colors}">${element.colors[0]}</option>
+        `;
+
+        document
+          .querySelector("section")
+          .insertAdjacentElement("beforeend", markup);
+      });
+    });
+};
+addColorToDropdown();
